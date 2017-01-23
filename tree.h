f155314459205b22594692c3a03e97bbb385ae3a
Thomas Boucard <thomas.boucard@epita.fr>
2017-01-23 16:35:48 +0100

    add description of function for tree

---
+++ tree.h
@@ -0,0 +1,41 @@
+//List of function used to manage a binary tree
+
+/*
+**  enumeration used to list type of node
+*/
+enum e_type
+{
+	operand,
+	function,
+	variable,
+	value
+};
+
+/*
+**  struct of BTree element
+**    left: the left child
+**    right: the right child
+**    type : the type of the data
+**    data: the data of the node
+*/
+struct          s_tree
+{
+	struct s_tree *left;
+	struct s_tree *right;
+	enum e_type   type;
+	void          *data;
+};
+
+/*
+**	create_node: create a node
+**		type: type of the data
+**		data: data of the node
+**	create_node create a node whose children are set to NULL
+*/
+struct s_tree *create_node(enum e_type type, void *data);
+
+/*
+**	free_tree: free a tree
+**	free_tree free all the tree and data in
+*/
+void free_tree(struct s_tree *tree);
