5d1d828ecb647b64ce582650df2274981bc912ca
Fernando Borja <fernando.borja@epita.fr>
2017-02-13 20:54:42 +0100

    List.c and .h, no test and algo not sure...List.c and .h, no test and algo not sure...

---
+++ list.c
@@ -0,0 +1,37 @@
+# include <stdio.h>
+# include "list.h"
+
+void queue_init(struct queue *queue)
+{
+  queue->store = NULL;
+  queue->size = 0;
+}
+
+void queue_push(struct queue *queue, void *elm)
+{
+  struct list *tmp = NULL;
+
+  while(queue->store && queue->store->tree <= elm)
+  {
+    tmp = queue->store;
+    queue->store = queue->store->next;
+  }
+   tmp->next = queue->store->next;
+   queue->store->next = tmp;
+}
+
+
+struct s_tree *queue_pop(struct queue *queue)
+{
+  if(queue != NULL)
+  {
+    struct list *tmp = malloc(sizeof(struct list));
+    tmp = queue->store->next;
+    queue->store->next = tmp->next;
+    struct s_tree *test = tmp->tree;
+    free(tmp);
+    return test;
+  }
+  else
+    return NULL;
+}
---
+++ list.h
@@ -0,0 +1,40 @@
+//List of function used to make list
+
+# ifndef _EPIQUATION_LIST_H_
+# define _EPIQUATION_LIST_H_
+
+# include <stdlib.h>
+# include "tree.h"
+
+/* standard linked lists */
+struct list {
+  struct list   *next;
+  struct s_tree *tree;
+};
+
+/*
+ * queue container: replace sentinel and add abstraction
+ */
+struct queue {
+  struct list *store;
+  int       sum;
+};
+
+/*
+ * queue_init(queue) initialize an empty queue container
+ * allocation of the container is kept external for more flexibility
+ */
+void queue_init(struct queue *queue);
+
+/*
+ * queue_push(queue, elm) push elm
+ */
+void queue_push(struct queue *queue, void *elm);
+
+/*
+ * queue_pop(queue) pop the next element (FIFO order)
+ * returns NULL if the queue is empty
+ */
+struct s_tree *queue_pop(struct queue *queue);
+
+# endif
