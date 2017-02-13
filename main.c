4a55658a34fe702fb31f192542201732d036d9bd
Thomas Boucard <thomas.boucard39gmail.com>
2017-02-13 23:04:00 +0100

    add some test

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 # include "parsing.h"
+# include "test_tree.h"
 # include <stdio.h>
 
 int main()
 {
-	//char *s = "bonjour , (j'ai) envie de compteri AZE.RTY 1+ 2= 3 4 5 6 7 8 9 /*\"-&+";
-	//char *n = clean_string(s);
-	//printf("%s\n", n);
-
+  char *s = "pow(3 + y) + 56 * t + (5 - 9 /2 ) +34 = 5 - ln ( 4 *x + 6,3+5)";
+	char *n = clean_string(s);
+	printf("%s\n", n);
+  tree_to_string(parse(n));
 
 	return 0;
 }
