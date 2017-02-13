b81a95f23377de3481ea4d1b4918bf8eefdac93f
Thomas Boucard <thomas.boucard@epita.fr>
2017-02-13 02:00:25 +0100

    add main function

---
+++ main.c
@@ -0,0 +1,10 @@
+# include "parsing.h"
+# include <stdio.h>
+
+int main()
+{
+	char *s = "bonjour , (j'ai) envie de compteri AZE.RTY 1+ 2= 3 4 5 6 7 8 9 /*\"-&+";
+	char *n = clean_string(s);
+	printf("%s\n", n);
+	return 0;
+}
