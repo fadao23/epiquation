d26d8fba7351747893913bf179b10f9748e6f189
Thomas Boucard <thomas.boucard@epita.fr>
2017-03-09 23:39:53 +0100

    correct function option initialization

--- parsing.c
+++ parsing.c
@@ -67,7 +67,6 @@ struct s_tree *_parse(char *equation)
     {
       sub_string = calloc(strlen(equation + lgth + 1) + 1, sizeof (char));
       strcpy(sub_string, equation +lgth + 1);
-      //tree->right = _parse(sub_string);
       sscanf(sub_string, "%f", &((struct s_function*) tree->data)->parameter);
 			free(sub_string);
     }
