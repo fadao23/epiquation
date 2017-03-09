9a2df53c9cff5b76db94832b2a52106ae27c381b
Thomas Boucard <thomas.boucard@epita.fr>
2017-03-09 12:28:47 +0100

    update creation of node

--- build.c
+++ build.c
@@ -1,35 +1,42 @@
 # include "build.h"
 
-struct s_tree *build_operator(char oper)
+struct s_tree *build(void)
 {
   struct s_tree *node = malloc(sizeof (struct s_tree));
+	node->left = NULL;
+	node->right = NULL;
+	return node;
+}
+struct s_tree *build_operator(char oper)
+{
+  struct s_tree *node = build();
   node->type = OPERAND;
   node->data = get_operator(oper);
-  return node;
+	return node;
 }
 
 struct s_tree *build_function(char *func)
 {
-  struct s_tree *node = malloc(sizeof (struct s_tree));
+  struct s_tree *node = build();
   node->type = FUNCTION;
   node->data = init_function(get_function(func), 0);
-  return node;
+	return node;
 }
 
 struct s_tree *build_variable(char *var)
 {
-  struct s_tree *node = malloc(sizeof (struct s_tree));
+  struct s_tree *node = build();
   node->type = VARIABLE;
   node->data = init_variable(*var);
-  return node;
+ 	return node;
 }
 
 struct s_tree *build_number(char *number)
 {
-  struct s_tree *node = malloc(sizeof (struct s_tree));
+  struct s_tree *node = build();
   float *res = malloc(sizeof (float));
   sscanf(number, "%f", res);
   node->type = VALUE;
   node->data = res;
-  return node;
+ 	return node;
 }
