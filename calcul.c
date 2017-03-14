c44a52c74e1881095afe1f8d7cfeba11729e1dd3
Thomas Boucard <thomas.boucard39gmail.com>
2017-03-14 15:54:00 +0100

    simplify multiplication tree

--- calcul.c
+++ calcul.c
@@ -1,61 +1,20 @@
 # include "calcul.h"
 
-void multiplie_tree(struct s_tree **node, float coef, int bol)
+void multiplie_tree(struct s_tree *node, float coef)
 {
-
-  float r;
-  struct s_tree *tmp;
   if (node == NULL || coef == 1)
     return;
-  if (!bol)
+
+  if (node->type == OPERAND)
   {
-    if ((*node)->type == OPERAND && *((enum e_operator*)(*node)->data) == TIME)
-    {
-      if (((*node)->left->type) == VALUE)
-      {
-        r = *((float*)(*node)->left->data);
-        multiplie_tree(&(*node)->right,r,1);
-        tmp = *node;
-        *node = (*node)->right;
-        tmp->right = NULL;
-        free_tree(tmp);
-        return;
-      }
-      if((*node)->right->type == VALUE)
-      {
-        r = *((float*)(*node)->right->data);
-        multiplie_tree(&(*node)->left,r,1);
-        tmp = *node;
-        *node = (*node)->left;
-        tmp->left = NULL;
-        free_tree(tmp);
-        return;
-      }
-    }
-    multiplie_tree(&(*node)->right,coef,0);
-    multiplie_tree(&(*node)->left,coef,0);
+    if(*((enum e_operator*)node->data) == PLUS)
+      multiplie_tree(node->right,coef);
+    multiplie_tree(node->left,coef);
   }
-  else
-  {
-    if ((*node)->type == OPERAND)
-    {
-      if(*((enum e_operator*)(*node)->data) == MINUS  || *((enum
-              e_operator*)(*node)->data) == PLUS){
-        multiplie_tree(&(*node)->right,coef,1);
-      }
-      multiplie_tree(&(*node)->left,coef,1);
-    }
-    if ((*node)->type == VALUE)
-    {
-      (*(float*)(*node)->data) *= coef;
-    }
-    if ((*node)->type == VARIABLE)
-    {
-      ((struct s_variable*)(*node)->data)->mult *= coef;
-    }
-    if ((*node)->type == FUNCTION)
-    {
-      ((struct s_function*)(*node)->data)->mult *= coef;
-    }
-  }
+  else if (node->type == VALUE)
+    (*(float*)node->data) *= coef;
+  else if (node->type == VARIABLE)
+    ((struct s_variable*)node->data)->mult *= coef;
+  else if (node->type == FUNCTION)
+    ((struct s_function*)node->data)->mult *= coef;
 }
--- calcul.h
+++ calcul.h
@@ -14,6 +14,6 @@
 **  in function example : 4(ax + b) = 4*ax + 4*b
 **  After, the function remove children which had the number
 */
-void multiplie_tree(struct s_tree **node, float coef, int bol);
+void multiplie_tree(struct s_tree *node, float coef);
 
 # endif /* _EPIQUATION_CALCUL_H_ */
