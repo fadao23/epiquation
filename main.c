65578638f80003a39b53b809aaa2be0934270bca
Fernando Borja <fernando.borja@epita.fr>
2017-04-20 15:23:05 +0200

    Coms

--- main.c
+++ main.c
@@ -6,27 +6,31 @@
 # include <err.h>
 # include "interface.h"
 # include "parsing.h"
-//void on_quit_clicked();
 
-void sol(char *arg);
 
 int main(int argc,char **argv)
 {
-  /* Déclaration du widget */
+  /* Iniliasition of widget */
   gtk_init(&argc,&argv);
 
+  /* Initiatlisation */
   builder = gtk_builder_new();
   gtk_builder_add_from_file(builder, "interface.glade", NULL);
 
+  /* Initialisation of Mainwindow and stack */
   MainWindow = GTK_WIDGET(gtk_builder_get_object(builder,"MainWindow"));
   stack = GTK_WIDGET(gtk_builder_get_object(builder,"stack"));
-  //stack = GTK_WIDGET(gtk_builder_get_object(builder,"stack"));
-  //stack = gtk_stack_new ();
+
+  /* Setting signals */
   gtk_builder_connect_signals(builder,NULL);
 
+  /* Setting entry text zone for Affine and out text zone*/
   entry = GTK_ENTRY(gtk_builder_get_object(builder,"text_in"));
   out = GTK_LABEL(gtk_builder_get_object(builder,"text_out"));
   out2 = GTK_LABEL(gtk_builder_get_object(builder,"text_out2"));
+
+
+  /* Build and set window*/
   g_object_unref (G_OBJECT (builder));
   gtk_widget_show(MainWindow);
   gtk_main();
@@ -34,6 +38,7 @@ int main(int argc,char **argv)
   return 0;
 }
 
+/* ---------------------- QUIT && RETURN BUTTONS ---------------- */
 void on_quit1_clicked()
 {
   gtk_main_quit();
@@ -42,12 +47,38 @@ void on_quit2_clicked()
 {
   gtk_main_quit();
 }
+void on_quit3_clicked()
+{
+  gtk_main_quit();
+}
 
+void on_return1_clicked()
+{
+  gtk_stack_set_visible_child_name(GTK_STACK(stack),"page1");
+}
+
+void on_return2_clicked()
+{
+  gtk_stack_set_visible_child_name(GTK_STACK(stack),"page1");
+}
+/* --------------------------------------------------------------*/
+
+
+/* ------------------------ BUTTONS MENU ------------------------*/
 void on_Baffine_clicked()
 {
   gtk_stack_set_visible_child_name(GTK_STACK(stack),"page0");
 }
 
+void on_polynomes_clicked()
+{
+  gtk_stack_set_visible_child_name(GTK_STACK(stack),"page2");
+}
+/* --------------------------------------------------------------*/
+
+
+
+/* --------------------------- AFFINE ----------------------------*/
 void on_cancel_clicked()
 {
   const gchar *dtr = "";
@@ -110,3 +141,4 @@ void sol(char *arg) {
   free(re);
   set_erreur(0);
 }
+/* --------------------------------------------------------------*/
