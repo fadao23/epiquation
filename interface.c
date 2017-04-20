b208076fb77709d8ea813106eb8ed597afa1f123
Fernando Borja <fernando.borja@epita.fr>
2017-04-20 16:49:35 +0200

    com in interface.c

--- interface.c
+++ interface.c
@@ -27,19 +27,19 @@ char *solve_system(char **system, int n) {
   return s;
 }
 
-int main() {                                                                    
-  float c[3][3] = {{1,3,6},{2,8,16},{5,21,45}};                                 
-  float *a = malloc(9*sizeof (int));                                            
-  for (int i = 0; i < 3; i++) {                                                 
-    for (int j = 0; j <3; ++j)                                                  
-      *(a+i*3+j) = c[i][j];                                                     
-  }                      
+/*int main() {
+  float c[3][3] = {{1,3,6},{2,8,16},{5,21,45}};
+  float *a = malloc(9*sizeof (int));
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j <3; ++j)
+      *(a+i*3+j) = c[i][j];
+  }
   char **system = malloc(3 * sizeof (char*));
   *system = "1x+2b+5c=1";
   *(system + 1) = "3x + 8b + 21c = 2";
   *(system + 2) = "6x + 16b + 45c = 3";
   char *s = solve_system(system, 3);
   printf("%s", s);
-  
-  return 0;      
-}
+
+  return 0;
+}*/
