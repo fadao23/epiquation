76896ab4532299f106cdb3170d05221fc2513a13
Lucas Rangeard <lucas.rangeard@epita.fr>
2017-05-27 18:24:29 +0200

    deriv

--- derivate.c
+++ derivate.c
@@ -1,17 +1,86 @@
-
-
-struct s_tree *deriv_var(struct s_tree *node)
+int contain_var (struct s_tree *node)
 {
-	new = build_variable(node->data->name);
+  if (node)
+		return 0;
+	if (node->type != VARIABLE)
+		return contain_var(node->left) || contain_var(node->right);
+	return 1;
+}
+
+
+struct s_tree *deriv (struct s_tree *node)
+{
+	if (node->type == VARIABLE)
+		return deriv_var(node);
+  if (node->type == OPERAND)
+    return deriv_op(node);
+  if (node->type == FUNCTION)
+    return deriv_func(node);
+	return NULL;
+}
+
+/*
+struct s_tree *deriv_func (struct s_tree *node)
+{
+  struct s_tree *new;
+  if (node->data->funcction == EXP)
+	{
+		new = build_operator("*");
+		new->left = deriv(node->data->param) * node->data->mult;
+		new->right = node;
+	}
+	if (node->data->funcction == LN)
+	{
+		new = build_operator("*");
+		new->left = deriv(node->data->param) * node->data->mult; //mult ln(2x^2) ??
+		new->right-> = node->data->param; // ?
+		new->right->power = -1;
+	}
+	if (node->data->funcction == SQRT || POW)
+	{
+		new = build_operator("*");
+		new->left = deriv(node->data->param)
+	}
+}
+*/
+
+struct s_tree *deriv_var (struct s_tree *node)
+{
+	struct s_tree *new = build_variable(node->data->name);
 	new->data->mult = node->data->mult * node->data->power;
 	new->data->power = node->data->power - 1;
 	return new;
 }
 
 
-struct s_tree *deriv_op(struct s_tree *node)
+struct s_tree *deriv_op (struct s_tree *node)
 {
-	new = build_operator(node->data->name);
+	struct s_tree *new;
+	if (*(node->data) == TIME)
+	{
+		new = build_operator("+");
+		new->left = build_operator("*");
+		new->left->left = deriv(node->left);
+		new->left->right = node->right;
 	
-	return new;	
+		new->right = build_operator("*");
+		new->right->left = deriv(node->right);
+    new->right->right = node->left;
+	}
+	else
+	{
+		struct s_tree *left, *right;
+		if (contain_var(node->left))
+			left = deriv(node->left);
+		if (contain_var(node->right))
+			right = deriv(node->right);
+		if (left && right) {
+			new = build_operator('+');
+			new->left = left;
+			new->right = right;
+		}
+		else
+			new = (right)? right : left;
+	}
+	return new;
 }
